0c813bb6f03f9134dd91fcc6e9f3c9d1bb7dc770
Md Kawser Ishtiak Julas <116008940+mdkawserishtiak@users.noreply.github.com>
2026-08-24 19:00:50 +0600

    Add function of playing the game onetime

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ const getComputerChoice = () => {
 const getHumanChoice = () => {
   return prompt("What is your choice: (Rock, Paper, Scissors)");
 };
+
+let humanScore = 0;
+let computerScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log("Tie!");
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats Rock");
+
+    computerScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose! Scissors beats Paper");
+
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock beats Scissors");
+
+    computerScore++;
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You won! Rock beats Scissors");
+
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You won! Paper beats rock");
+
+    humanScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You won! Scissors beats Paper");
+
+    humanScore++;
+  }
+};
