e678e0285e5ef0ed0c3a575c2785ef93f5c9b24d
Md Kawser Ishtiak Julas <116008940+mdkawserishtiak@users.noreply.github.com>
2026-08-24 18:20:57 +0600

    Add function of random computer choice

---
+++ script.js
@@ -0,0 +1,11 @@
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
+
+  if (randomNumber === 1) {
+    return "rock";
+  } else if (randomNumber === 2) {
+    return "paper";
+  } else if (randomNumber === 3) {
+    return "scissors";
+  }
+};
